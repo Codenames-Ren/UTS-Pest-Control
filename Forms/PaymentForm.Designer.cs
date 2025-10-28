a8fc038f40a8ad032e803858232563aaca8de4ab
rayhan-18 <mr0031689@gmail.com>
2025-10-28 14:45:05 +0700

    nambah kode payment

--- Forms/PaymentForm.Designer.cs
+++ Forms/PaymentForm.Designer.cs
@@ -1,4 +1,8 @@
-﻿namespace UTS_Pest_Control.Forms
+﻿
+using System.IO.Packaging;
+using UTS_Pest_Control.Models;
+
+namespace UTS_Pest_Control.Forms
 {
     partial class PaymentForm
     {
@@ -47,121 +51,135 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(39, 23);
+            label1.Location = new Point(34, 17);
             label1.Name = "label1";
-            label1.Size = new Size(47, 20);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
             label1.Text = "Client";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(39, 88);
+            label2.Location = new Point(34, 66);
             label2.Name = "label2";
-            label2.Size = new Size(63, 20);
+            label2.Size = new Size(51, 15);
             label2.TabIndex = 1;
             label2.Text = "Package";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(485, 20);
+            label3.Location = new Point(424, 15);
             label3.Name = "label3";
-            label3.Size = new Size(121, 20);
+            label3.Size = new Size(99, 15);
             label3.TabIndex = 2;
             label3.Text = "Payment Method";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(564, 91);
+            label4.Location = new Point(494, 68);
             label4.Name = "label4";
-            label4.Size = new Size(42, 20);
+            label4.Size = new Size(33, 15);
             label4.TabIndex = 3;
             label4.Text = "Total";
             // 
             // cmbClient
             // 
             cmbClient.FormattingEnabled = true;
-            cmbClient.Location = new Point(123, 20);
+            cmbClient.Location = new Point(108, 15);
+            cmbClient.Margin = new Padding(3, 2, 3, 2);
             cmbClient.Name = "cmbClient";
-            cmbClient.Size = new Size(264, 28);
+            cmbClient.Size = new Size(232, 23);
             cmbClient.TabIndex = 4;
             // 
             // cmbPackage
             // 
             cmbPackage.FormattingEnabled = true;
-            cmbPackage.Location = new Point(123, 83);
+            cmbPackage.Location = new Point(108, 62);
+            cmbPackage.Margin = new Padding(3, 2, 3, 2);
             cmbPackage.Name = "cmbPackage";
-            cmbPackage.Size = new Size(264, 28);
+            cmbPackage.Size = new Size(232, 23);
             cmbPackage.TabIndex = 5;
             // 
             // cmbMethod
             // 
             cmbMethod.FormattingEnabled = true;
             cmbMethod.Items.AddRange(new object[] { "Cash", "Transfer Bank", "E-Wallet" });
-            cmbMethod.Location = new Point(641, 20);
+            cmbMethod.Location = new Point(561, 15);
+            cmbMethod.Margin = new Padding(3, 2, 3, 2);
             cmbMethod.Name = "cmbMethod";
-            cmbMethod.Size = new Size(264, 28);
+            cmbMethod.Size = new Size(232, 23);
             cmbMethod.TabIndex = 6;
             // 
             // txtTotal
             // 
-            txtTotal.Location = new Point(641, 87);
+            txtTotal.Location = new Point(561, 65);
+            txtTotal.Margin = new Padding(3, 2, 3, 2);
             txtTotal.Name = "txtTotal";
-            txtTotal.Size = new Size(264, 27);
+            txtTotal.Size = new Size(232, 23);
             txtTotal.TabIndex = 7;
             // 
             // btnClear
             // 
-            btnClear.Location = new Point(584, 158);
+            btnClear.Location = new Point(511, 118);
+            btnClear.Margin = new Padding(3, 2, 3, 2);
             btnClear.Name = "btnClear";
-            btnClear.Size = new Size(94, 39);
+            btnClear.Size = new Size(82, 29);
             btnClear.TabIndex = 47;
             btnClear.Text = "Clear";
             btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
             // 
             // btnDelete
             // 
-            btnDelete.Location = new Point(454, 158);
+            btnDelete.Location = new Point(397, 118);
+            btnDelete.Margin = new Padding(3, 2, 3, 2);
             btnDelete.Name = "btnDelete";
-            btnDelete.Size = new Size(94, 39);
+            btnDelete.Size = new Size(82, 29);
             btnDelete.TabIndex = 46;
             btnDelete.Text = "Delete";
             btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
             // 
             // btnUpdate
             // 
-            btnUpdate.Location = new Point(305, 158);
+            btnUpdate.Location = new Point(267, 118);
+            btnUpdate.Margin = new Padding(3, 2, 3, 2);
             btnUpdate.Name = "btnUpdate";
-            btnUpdate.Size = new Size(94, 39);
+            btnUpdate.Size = new Size(82, 29);
             btnUpdate.TabIndex = 45;
             btnUpdate.Text = "Update";
             btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(159, 158);
+            btnAdd.Location = new Point(139, 118);
+            btnAdd.Margin = new Padding(3, 2, 3, 2);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(94, 39);
+            btnAdd.Size = new Size(82, 29);
             btnAdd.TabIndex = 44;
             btnAdd.Text = "Add";
             btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
             // 
             // dgvPayments
             // 
             dgvPayments.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvPayments.Location = new Point(35, 220);
+            dgvPayments.Location = new Point(31, 165);
+            dgvPayments.Margin = new Padding(3, 2, 3, 2);
             dgvPayments.Name = "dgvPayments";
             dgvPayments.RowHeadersWidth = 51;
-            dgvPayments.Size = new Size(922, 380);
+            dgvPayments.Size = new Size(807, 285);
             dgvPayments.TabIndex = 48;
+            dgvPayments.CellContentClick += dgvPayments_CellContentClick;
             // 
             // PaymentForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(990, 617);
+            ClientSize = new Size(866, 463);
             Controls.Add(dgvPayments);
             Controls.Add(btnClear);
             Controls.Add(btnDelete);
@@ -175,11 +193,53 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "PaymentForm";
             Text = "PaymentForm";
+            Load += PaymentForm_Load;
             ((System.ComponentModel.ISupportInitialize)dgvPayments).EndInit();
             ResumeLayout(false);
             PerformLayout();
+        }
+
+        private void PaymentForm_Load(object sender, EventArgs e)
+        {
+            LoadClients();
+            LoadPackagdes();
+            LoadPayments();
+
+            dgvPayments.SelectionMode = DataGridViewSelectionMode.FullRowSelect;dgvPayments.MultiSelect = false;
+        }
+
+        private void LoadClients()
+        {
+            var clients = _packageService.GetAllPackages().ToList();
+            cmbPackage.DataSource = Package;
+            cmbPackage.DisplayMember = "Package Name";
+            cmbPackage.ValueMember = "Package ID";
+        }
+
+        private void LoadPayments()
+        {
+            dgvPayments.DataSource = _packageService.GetAllPayments()
+                .Select(p => new
+                {
+                    p.PaymentID,
+                    ClientName = p.Client?.Name,
+                    PackageName = p.Package?.PackageName,
+                    p.PaymentMethod,
+                    p.TotalAmaount,
+                    p,
+                    PaymentDate
+                }).ToList();
+        }
+
+        private void ClearInputs()
+        {
+            cmbClient.SelectedIndex = -1;
+            cmbPackage.SelectedIndex = -1;
+            cmbMethod.SelectedIndex = -1;
+            txtTotal.Text = "";
         }
 
         #endregion
--- Forms/PaymentForm.cs
+++ Forms/PaymentForm.cs
@@ -7,14 +7,119 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UTS_Pest_Control.Models;
+using UTS_Pest_Control.Services;
 
 namespace UTS_Pest_Control.Forms
 {
     public partial class PaymentForm : Form
     {
-        public PaymentForm()
+        private readonly PaymentService _paymentService;
+        private readonly ClientService _clientService;
+        private readonly PackageService _packageService;
+        public PaymentForm(PaymentService paymentService, ClientService clientService, PackageService packageService)
         {
             InitializeComponent();
+            _paymentService = paymentService;
+            _clientService = clientService;
+            _packageService = packageService;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cmbClient.SelectedIndex == -1 || cmbPackage.SelectedIndex == -1 || cmbMethod.SelectedIndex == -1 || string.IsNullOrWhiteSpace(txtTotal.Text.Trim()))
+            {
+                MessageBox.Show("Semua field harus diisi!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (!decimal.TryParse(txtTotal.Text.Trim(), out decimal total))
+            {
+                MessageBox.Show("Total harus berupa angka!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var payment = new Payment
+            {
+                ClientId = (int)cmbClient.SelectedValue,
+                PaymentMethod = cmbMethod.SelectedItem.ToString(),
+                TotalAmount = total,
+                PaymentDate = DateTime.Now
+            };
+
+            _paymentService.AddPayment(payment);
+            MessageBox.Show("Pembayaran berhasil ditambahkan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadPayments();
+            ClearInputs();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvPayments.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Pilih transaksi yang ingin diupdate.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var selectedId = (int)dgvPayments.SelectedRows[0].Cells["PaymentID"].Value;
+
+            if (!decimal.TryParse(txtTotal.Text.Trim(), out decimal total))
+            {
+                MessageBox.Show("Total harus berupa angka!", "Validasi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var payment = new Payment
+            {
+                PaymentID = selectedId,
+                ClientID = (int)cmbClient.SelectedValue,
+                PackageID = (int)cmbPackage.SelectedValue,
+                PaymentMethod = cmbMethod.SelectedItem.ToString(),
+                TotalAmount = total,
+                PaymentDate = DateTime.Now
+            };
+
+            _paymentService.UpdatePayment(payment);
+            MessageBox.Show("Transaksi berhasil diupdate!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            LoadPayments();
+            ClearInputs();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvPayments.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Pilih transaksi yang ingin dihapus.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            var selectedId = (int)dgvPayments.SelectedRows[0].Cells["PaymentID"].Value;
+            var confirm = MessageBox.Show("Yakin ingin menghapus transaksi ini?", "Konfirmasi", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirm == DialogResult.Yes)
+            {
+                _paymentService.DeletePayment(selectedId);
+                MessageBox.Show("Transaksi berhasil dihapus!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LoadPayments();
+                ClearInputs();
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInputs();
+        }
+
+        private void dgvPayments_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var row = dgvPayments.Rows[e.RowIndex];
+                cmbClient.Text = row.Cells["ClientName"].Value?.ToString();
+                cmbPackage.Text = row.Cells["PackageName"].Value?.ToString();
+                cmbMethod.Text = row.Cells["PaymentMethod"].Value?.ToString();
+                txtTotal.Text = row.Cells["TotalAmount"].Value?.ToString();
+            }
         }
     }
 }
