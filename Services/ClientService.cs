b766a94ae7219aa3d20126c3d82dcf59b07b3811
Codenames-Ren <renmizuki38@gmail.com>
2025-10-28 11:45:49 +0700

    Nambah service Client sama Package

---
+++ Services/ClientService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTS_Pest_Control.Models;
+using UTS_Pest_Control.Repositories;
+
+namespace UTS_Pest_Control.Services
+{
+    public class ClientService
+    {
+        private readonly ClientRepository _clientRepository;
+
+        public ClientService(ClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public IEnumerable<Client> GetAllClients()
+        {
+            return _clientRepository.GetAll();
+        }
+        public Client GetClientById(int id) 
+        {
+            return _clientRepository.GetById(id);
+        }
+        public void AddClient(Client client) 
+        {
+            //Validasi Input
+            client.Name = client.Name?.Trim();
+            client.Address = client.Address?.Trim();
+            client.Phone = client.Phone?.Trim();
+
+            if (string.IsNullOrWhiteSpace(client.Name)) throw new System.Exception("Nama Client tidak boleh kosong!");
+            if (string.IsNullOrWhiteSpace(client.Address)) throw new System.Exception("Alamat tidak boleh kosong!");
+            if (string.IsNullOrWhiteSpace(client.Phone)) throw new System.Exception("Nomor telepon tidak boleh kosong!");
+            _clientRepository.Add(client);
+        }
+
+        public void UpdateClient(Client client) 
+        {
+            client.Name = client.Name?.Trim();
+            client.Address = client.Address?.Trim();
+            client.Phone = client.Phone?.Trim();
+            
+            if (string.IsNullOrWhiteSpace(client.Name)) throw new System.Exception("Nama Client tidak boleh kosong!");
+            if (string.IsNullOrWhiteSpace(client.Address)) throw new System.Exception("Alamat tidak boleh kosong!");
+            if (string.IsNullOrWhiteSpace(client.Phone)) throw new System.Exception("Nomor telepon tidak boleh kosong!");
+            _clientRepository.Update(client);
+        }
+
+        public void DeleteClient(int id) 
+        {
+            _clientRepository.Delete(id);
+        }
+    }
+}
---
+++ Services/PackageService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UTS_Pest_Control.Models;
+using UTS_Pest_Control.Repositories;
+
+namespace UTS_Pest_Control.Services
+{
+    public class PackageService
+    {
+        private readonly PackageRepository _packageRepository;
+        
+        public PackageService(PackageRepository packageRepository) 
+        {
+            _packageRepository = packageRepository;
+        }
+        public IEnumerable<Package> GetAllPackages() 
+        {
+            return _packageRepository.GetAll();
+        }
+        public Package GetPackageById(int id) 
+        {
+            return _packageRepository.GetById(id);
+        }
+        public void AddPackage(Package package) 
+        {
+            package.Name = package.Name?.Trim();
+            package.Description = package.Description?.Trim();
+
+            if (string.IsNullOrWhiteSpace(package.Name)) throw new System.Exception("Nama paket tidak boleh kosong!");
+            if (string.IsNullOrWhiteSpace(package.Description)) throw new System.Exception("Deskripsi paket tidak boleh kosong!");
+            if (package.Price <= 0) throw new System.Exception("Harga paket harus lebih dari 0!");
+            _packageRepository.Add(package);
+        }
+
+        public void UpdatePackage(Package package) 
+        {
+            package.Name = package.Name?.Trim();
+            package.Description = package.Description?.Trim();
+
+            if (string.IsNullOrWhiteSpace(package.Name)) throw new System.Exception("Nama paket tidak boleh kosong!");
+            if (string.IsNullOrWhiteSpace(package.Description)) throw new System.Exception("Deskripsi paket tidak boleh kosong!");
+            if (package.Price <= 0) throw new System.Exception("Harga paket harus lebih dari 0!");
+            _packageRepository.Update(package);
+        }
+        public void DeletePackage(int id) 
+        {
+            _packageRepository.Delete(id);
+        }
+    }
+}
